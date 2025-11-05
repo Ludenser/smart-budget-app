3db669e91eefdf6172f16a2b022ea0b8f8b8cc81
ludenser <served9@yandex.ru>
2025-11-05 15:56:50 +0400

    fix

--- apps/web/middleware/protected.ts
+++ apps/web/middleware/protected.ts
@@ -3,7 +3,6 @@ export default defineNuxtRouteMiddleware(async (to) => {
 
   // Проверяем авторизацию для защищенных маршрутов
   await getSession();
-
   if (status.value !== 'authenticated') {
     return navigateTo('/login');
   }
